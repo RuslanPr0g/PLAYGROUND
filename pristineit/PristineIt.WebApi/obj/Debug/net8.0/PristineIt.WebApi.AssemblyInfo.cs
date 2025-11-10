45ea415e8fec63b2e612626274d37cafbd9bce35
RuslanPr0g <theruslan.prog@gmail.com>
2025-11-10 14:28:29 +0100

    add references

--- pristineit/PristineIt.WebApi/obj/Debug/net8.0/PristineIt.WebApi.AssemblyInfo.cs
+++ pristineit/PristineIt.WebApi/obj/Debug/net8.0/PristineIt.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PristineIt.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f026da3c09b09c3fe48bc7bb87889860605275cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c8e70752303abcea618181041b2c2cc52450279")]
 [assembly: System.Reflection.AssemblyProductAttribute("PristineIt.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PristineIt.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
