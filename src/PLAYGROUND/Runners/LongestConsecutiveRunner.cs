eea8b0cb1a0b8c53e56f7758dc7730238e2aabc6
RuslanPr0g <theruslan.prog@gmail.com>
2026-06-14 19:44:04 +0200

    add more problems

---
+++ src/PLAYGROUND/Runners/LongestConsecutiveRunner.cs
@@ -0,0 +1,38 @@
+﻿namespace Playground.Runners;
+
+internal class LongestConsecutiveRunner : RunnerBase
+{
+    public override string Description => "XXXXX";
+
+    public override ValueTask Run(IUserPrompt prompt)
+    {
+        LongestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]);
+        return ValueTask.CompletedTask;
+    }
+
+    public int LongestConsecutive(int[] nums)
+    {
+        var set = nums.ToHashSet();
+        var max = 0;
+
+        foreach (var item in set)
+        {
+            if (set.Contains(item - 1))
+            {
+                continue;
+            }
+
+            var acc = 0;
+            var itenc = item;
+
+            while (set.Contains(itenc++))
+            {
+                acc++;
+            }
+
+            max = Math.Max(max, acc);
+        }
+
+        return max;
+    }
+}
--- src/PLAYGROUND/Runners/ProductExceptSelfRunner.cs
+++ src/PLAYGROUND/Runners/ProductExceptSelfRunner.cs
@@ -6,11 +6,38 @@ internal class ProductExceptSelfRunner : RunnerBase
 
     public override ValueTask Run(IUserPrompt prompt)
     {
+        ProductExceptSelf([1, 2, 3, 4]);
         return ValueTask.CompletedTask;
     }
 
     public int[] ProductExceptSelf(int[] nums)
     {
+        var left = new int[nums.Length];
+        var right = new int[nums.Length];
+        var accumulator = 1;
+
+        left[0] = 1;
+        right[nums.Length - 1] = 1;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            left[i] = accumulator * nums[i - 1];
+            accumulator *= nums[i - 1];
+        }
+
+        accumulator = 1;
+
+        for (var i = nums.Length - 2; i >= 0; i--)
+        {
+            right[i] = accumulator * nums[i + 1];
+            accumulator *= nums[i + 1];
+        }
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            nums[i] = left[i] * right[i];
+        }
+
         return nums;
     }
 }
