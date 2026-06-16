80396e8e46062866cef5660c7a6ee9fab3e05319
RuslanPr0g <theruslan.prog@gmail.com>
2026-06-16 11:34:46 +0200

    feat: a few more problems added

---
+++ src/PLAYGROUND/Runners/IsPalindromeRunner.cs
@@ -0,0 +1,43 @@
+﻿namespace Playground.Runners;
+
+internal class IsPalindromeRunner : RunnerBase
+{
+    public override string Description => "XXXXX";
+
+    public override ValueTask Run(IUserPrompt prompt)
+    {
+        IsPalindrome("A man, a plan, a canal: Panama");
+        return ValueTask.CompletedTask;
+    }
+
+    public bool IsPalindrome(string s)
+    {
+        for (int i = 0, j = s.Length - 1; i < s.Length && j >= i;)
+        {
+            var a = char.ToLower(s[i]);
+            var b = char.ToLower(s[j]);
+
+            if (!char.IsLetterOrDigit(a))
+            {
+                i++;
+                continue;
+            }
+
+            if (!char.IsLetterOrDigit(b))
+            {
+                j--;
+                continue;
+            }
+
+            if (a != b)
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
---
+++ src/PLAYGROUND/Runners/ThreeSumRunner.cs
@@ -0,0 +1,51 @@
+﻿namespace Playground.Runners;
+
+internal class ThreeSumRunner : RunnerBase
+{
+    public override string Description => "XXXXX";
+
+    public override ValueTask Run(IUserPrompt prompt)
+    {
+        var result = ThreeSum([2, -3, 0, -2, -5, -5, -4, 1, 2, -2, 2, 0, 2, -4, 5, 5, -10]);
+        return ValueTask.CompletedTask;
+    }
+
+    public IList<IList<int>> ThreeSum(int[] nums)
+    {
+        var result = new List<IList<int>>();
+        nums.Sort();
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            var left = i + 1;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
+                {
+                    result.Add([nums[i], nums[left], nums[right]]);
+
+                    while (left < right && nums[left] == nums[left + 1])
+                        left++;
+
+                    while (left < right && nums[right] == nums[right - 1])
+                        right--;
+
+                    left++;
+                    right--;
+                }
+
+                if (sum < 0) left++;
+
+                if (sum > 0) right--;
+            }
+        }
+
+        return result;
+    }
+}
