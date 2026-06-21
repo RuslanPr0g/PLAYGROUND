6edc6d15cca2f982ec9adc08b3c08dab6d6a43c7
Ruslan Rudenko <theruslan.prog@gmail.com>
2026-06-21 12:34:51 +0200

    feat: remove nth node from end of linked list

---
+++ src/PLAYGROUND/Runners/RemoveNthNodeRunner.cs
@@ -0,0 +1,44 @@
+namespace Playground.Runners;
+
+public class ListNodeRemoveNth(int val = 0, ListNodeRemoveNth? next = null)
+{
+  public int val = val;
+  public ListNodeRemoveNth? next = next;
+}
+
+internal class RemoveNthFromEndRunner : RunnerBase
+{
+  public override string Description => "remove nth node";
+
+  public ListNodeRemoveNth? RemoveNthFromEnd(ListNodeRemoveNth? head, int n)
+  {
+    var dict = new Dictionary<int, ListNodeRemoveNth>();
+    int i = -1;
+    var start = head;
+
+    while (head is not null)
+    {
+      i++;
+      dict.Add(i, head);
+      head = head.next;
+    }
+
+    dict.TryGetValue(i - n, out var prev);
+    dict.TryGetValue(i - n + 2, out var next);
+
+    if (prev is null)
+    {
+      return next;
+    }
+
+    prev.next = next;
+
+    return start;
+  }
+
+  public override ValueTask Run(IUserPrompt prompt)
+  {
+    RemoveNthFromEnd(new(), 1);
+    return ValueTask.CompletedTask;
+  }
+}
