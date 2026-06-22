f04fc06f32f78017f667d2ef3c1e3d29b58b62b7
Ruslan Rudenko <theruslan.prog@gmail.com>
2026-06-22 13:56:57 +0200

    feat: improve the sort n sorted lists (hard) to use built in prio queue

--- src/PLAYGROUND/Runners/MergeKSortedLists.cs
+++ src/PLAYGROUND/Runners/MergeKSortedLists.cs
@@ -1,24 +1,22 @@
 namespace Playground.Runners;
 
-// TODO: use min heap / prio queue
-
-public class ListNode(int val = 0, ListNode? next = null)
+public class ListNodeMergeK(int val = 0, ListNodeMergeK? next = null)
 {
   public int val = val;
-  public ListNode? next = next;
+  public ListNodeMergeK? next = next;
 }
 
 internal class MergeKListsRunner : RunnerBase
 {
   public override string Description => "MergeKLists";
 
-  public ListNode? MergeKLists(ListNode?[] lists)
+  public ListNodeMergeK? MergeKLists(ListNodeMergeK?[] lists)
   {
     var currMinNum = int.MaxValue;
     var currMinPos = -1;
-    ListNode? currMinNode = null;
-    ListNode start = new();
-    ListNode? ptr = start;
+    ListNodeMergeK? currMinNode = null;
+    ListNodeMergeK start = new();
+    ListNodeMergeK? ptr = start;
 
     while (lists.Any(v => v is not null))
     {
@@ -47,6 +45,35 @@ internal class MergeKListsRunner : RunnerBase
     }
 
     return start.next;
+  }
+
+  public ListNodeMergeK MergeKListsV2(ListNodeMergeK[] lists)
+  {
+    var prio = new PriorityQueue<ListNodeMergeK, int>();
+    var result = new ListNodeMergeK();
+    var iterator = result;
+
+    for (int i = 0; i < lists.Length; i++)
+    {
+      if (lists[i] is not null)
+      {
+        prio.Enqueue(lists[i], lists[i].val);
+      }
+    }
+
+    while (prio.Count > 0)
+    {
+      var el = prio.Dequeue();
+      iterator.next = el;
+      iterator = iterator.next;
+
+      if (el.next is not null)
+      {
+        prio.Enqueue(el.next, el.next.val);
+      }
+    }
+
+    return result.next!;
   }
 
   public override ValueTask Run(IUserPrompt prompt)
